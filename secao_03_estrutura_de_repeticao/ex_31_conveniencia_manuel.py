2bdb4d2c825facd41da2ec086fbedacd7c30bb22
Eliel Clementino da Silva <elielclementinodasilva@gmail.com>
2022-06-08 16:49:13 -0300

    resolvidos o exercício 31

--- secao_03_estrutura_de_repeticao/ex_31_conveniencia_manuel.py
+++ secao_03_estrutura_de_repeticao/ex_31_conveniencia_manuel.py
@@ -62,18 +62,18 @@ def rodar_programa_de_caixa():
     while True:
         choice = int(input("Digite 2 para continuar a executar o programa, -1 para finalizar a compra, e 0 para fechara a atual e iniciar outra: "))
         if choice == -1:
+            print("Lojas Tabajara")
+            print("-------------------")
+            print("Programa encerrado!")
             break
-        elif choice == 0:
-            valor_mercadoria = 0
-            dinheiro_pago = 0
-            troco = 0
-        elif choice == 2:
+        elif choice == 2 or choice == 0:
             valor_mercadoria = float(input("Digite o valor da mercadoria comprada"))
             dinheiro_pago = int(input("Dinheiro dado pelo cliente: "))
             troco = dinheiro_pago - valor_mercadoria
-    print("Lojas Tabajaras")
-    print(f"Total     : R$   {valor_mercadoria}")
-    print(f"Dinheiro  : R$   {dinheiro_pago}")
-    print(f"Troco     : R$   {troco}")
-    print("-------------------")
+            print("Lojas Tabajaras")
+            print(f"Total     : R$   {valor_mercadoria}")
+            print(f"Dinheiro  : R$   {dinheiro_pago}")
+            print(f"Troco     : R$   {troco}")
+            print("-------------------")
+            break
 
